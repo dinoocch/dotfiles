4cc06bcdb05efaa5134d646d40a569602bb28596
dinoocch <dino.occhialini@gmail.com>
2019-02-24 17:07:45 -0600

    Rust.el stuff

--- emacs/sources/languages/rust.el
+++ emacs/sources/languages/rust.el
@@ -1,5 +1,23 @@
 ;; rust (*.rs)
 
 (use-package rust-mode
-             :ensure t :pin melpa
+             :ensure t
              :mode "\\.rs\\'")
+
+(use-package racer
+             :ensure t
+             :diminish racer-mode
+             :config
+             (setq company-tooltip-align-annotations t
+                   racer-cmd (expand-file-name "~/.cargo/bin/racer"))
+             (add-hook 'rust-mode-hook #'racer-mode)
+             (add-hook 'racer-mode-hook #'eldoc-mode)
+             (add-hook 'racer-mode-hook #'company-mode)
+             (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+             :general
+             (
+              :keymaps 'rust-mode-map
+              :states 'insert
+              "TAB" #'company-indent-or-complete-common
+              )
+             )
