f29ec55187c99ea244ca8dfa7744abe2b446daf3
dinoocch <dino.occhialini@gmail.com>
2019-03-01 01:49:47 -0600

    Neotree arrow mode instead of icons

--- emacs/sources/ui/neotree.el
+++ emacs/sources/ui/neotree.el
@@ -1,12 +1,11 @@
-;; Neotree
 (use-package neotree
-             :ensure t
-             :config (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-             :after (general all-the-icons)
-             :general
-             (
-              :states 'normal
-              :prefix ","
-              "e" 'neotree-toggle
-              )
- )
+  :ensure t
+  :config (setq neo-theme 'arrow)
+  :after (:all general all-the-icons)
+  :general
+  (
+   :states 'normal
+   :prefix ","
+   "e" 'neotree-toggle
+   )
+  )
