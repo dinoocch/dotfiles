461788be2d59fa0f36f39f3a89d44ffd4b878446
dinoocch <dino.occhialini@gmail.com>
2020-01-15 13:53:22 -0800

    Fix dubious venv path changes

--- zsh/sources/aliases.zsh
+++ zsh/sources/aliases.zsh
@@ -27,9 +27,9 @@ if (( $+commands[git] )) {
     # If we're in a virtual env and
     if [ -n "${git_root}" ] && [ -z "$VIRTUAL_ENV" ]
     then
-      venv_bin=$(find "${git_root}" -path "*/venv/bin" | head -n 1)
+      venv_bin=$(find "${git_root}" -path "*/*venv*/bin/activate" | perl -e '@v = sort { length($a) <=> length($b) } <>; print $v[0]')
       if [ -n "$venv_bin" ]; then
-        . "${venv_bin}/activate"
+        . "${venv_bin}"
       fi
     fi
   }
@@ -41,7 +41,7 @@ if (( $+commands[git] )) {
     # If we're in a virtual env and
     if [ -n "${git_root}" ] && [ -z "$VIRTUAL_ENV" ]
     then
-      venv_bin=$(find "${git_root}" -path "*/venv/bin" | head -n 1)
+      venv_bin=$(find "${git_root}" -path "*/*venv*/bin/" | perl -e '@v = sort { length($a) <=> length($b) } <>; print $v[0]')
       if [ -n "$venv_bin" ]; then
         /usr/bin/env PATH="$venv_bin:$PATH" $EDITOR "$@"
       else
