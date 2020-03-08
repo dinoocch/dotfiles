b6c941390984eae97eaaaafb6dc640c20e94fd3c
dinoocch <dino.occhialini@gmail.com>
2020-03-07 21:34:19 -0800

    nvim-lsp working

--- .config/nvim/config/plugins/nvim-lsp.lua
+++ .config/nvim/config/plugins/nvim-lsp.lua
@@ -2,3 +2,23 @@
 
 -- vim.cmd('packadd nvim-lsp')
 require'nvim_lsp'.rust_analyzer.setup{}
+
+vim.api.nvim_set_keymap('n',
+	'gd', ':lua vim.lsp.buf.definition()<CR>',
+	{silent=true, noremap=true})
+
+vim.api.nvim_set_keymap('n',
+	'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>', {silent=true, noremap=true})
+
+vim.api.nvim_set_keymap('n',
+	'K', ':lua vim.lsp.buf.hover()<CR>',
+	{silent=true, noremap=true})
+
+vim.api.nvim_set_keymap('n',
+	'<c-k>', ':lua vim.lsp.buf.signature_help()<CR>',
+	{silent=true, noremap=true})
+
+vim.api.nvim_set_keymap('n',
+	'gr',
+	':lua vim.lsp.buf.references()<CR>',
+	{silent=true, noremap=true})
